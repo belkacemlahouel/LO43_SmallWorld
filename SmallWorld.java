76305e92a5b23698c9c58d49be870d7b86ba2f17
belka-x121exubuntu <belkacem.lahouel@gmail.com>
2013-12-10 21:30:37 +0100

    Creation of the main class

---
+++ SmallWorld.java
@@ -0,0 +1,16 @@
+
+/*
+ *	Class containing the main method
+ *	UTBM - LO43 - A2013
+ *	Lahouel Belkacem
+ *	Cadoret Luc - Le Morvan Valentin
+ *	
+*/
+
+public class SmallWorld {
+	public static void main (String[] args) {
+		System.out.println ("\tHello World!\n\n");
+		
+		System.out.println ("\n\tBye World!");
+	}
+}
