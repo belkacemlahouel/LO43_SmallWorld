7759388882ed86988e87eaa896d6235aba5a165d
belkacem-x121eThinkPad <belkacem.lahouel@gmail.com>
2014-01-02 19:15:10 +0100

    Correct printing of status during the game 	Adding the correct quantities 	Printing the type of Individuals in one tribe

--- src/gui/ResourcePanel.java
+++ src/gui/ResourcePanel.java
@@ -16,6 +16,7 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import kernel.Tribe;
 
 public class ResourcePanel extends JComponent{
 
@@ -35,25 +36,32 @@ public class ResourcePanel extends JComponent{
 		
 		for(int i=0;i<tribesResourceList.size();i++)
 		{
+			/*
+			 * @author Belkacem @date 02/01/14
+			 * Modification of the Resource Panel, printing all the information
+			 */
+			Tribe tmp = swGUI.getSw().getTribeAt(i);
 			tribesResourceList.get(i).setVisible(true);
 			tribesResourceList.get(i).setLayout(new GridLayout(6,1));;
-			tribesResourceList.get(i).add(new JLabel("Tribu " + (i+1)));
-			tribesResourceList.get(i).add(new JLabel("Bois : "));
-			tribesResourceList.get(i).add(new JLabel("Pierre : "));
-			tribesResourceList.get(i).add(new JLabel("Nourriture : "));
-			tribesResourceList.get(i).add(new JLabel("Plutonium : "));
-			tribesResourceList.get(i).add(new JLabel("Metal : "));
+			tribesResourceList.get(i).add(new JLabel("Tribu " + (i+1) + " (" + tmp.getIndividualType() + ")"));
+			tribesResourceList.get(i).add(new JLabel("Bois : " + tmp.getResources().get("Wood")));
+			tribesResourceList.get(i).add(new JLabel("Pierre : " + tmp.getResources().get("Rock")));
+			tribesResourceList.get(i).add(new JLabel("Nourriture : " + tmp.getResources().get("Food")));
+			tribesResourceList.get(i).add(new JLabel("Plutonium : " + tmp.getResources().get("Plutonium")));
+			tribesResourceList.get(i).add(new JLabel("Metal : " + tmp.getResources().get("Metal")));
 		}
 		
+		// OMG HOW WEIRD IS THAT? TODO
 		for(int i=0;i<tribesResourceList.size();i++)
 		{
 			this.add(tribesResourceList.get(i));
 		}
 		
 	}
-		protected void paintComponent(Graphics g) {
+	
+	/*protected void paintComponent(Graphics g) {
 		g.fillOval(100, 100, 100, 100);
-	}
+	}*/
 
 	
 }
